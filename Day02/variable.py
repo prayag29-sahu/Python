03a28677c275b6912eac025b0e968872c8186661
Prayag Sahu <sahuprayag229@gmail.com>
2025-08-31 19:34:12 +0530

    Variables

---
+++ Day02/variable.py
@@ -0,0 +1,36 @@
+a = 10
+print(a)
+print(type(a))
+
+name = "Prayag"
+print(name)
+
+b=2.6
+print(b)
+
+c = True
+print(c)
+
+a="10"
+print(a)
+print(type(a))
+
+print(a is a)
+
+c=10
+print(c)
+print(type(c))
+
+print(a is c)
+
+# 10
+# <class 'int' >
+# Prayag
+# 2.6
+# True
+# 10
+# <class 'str' >
+# True
+# 10
+# <class 'int' >
+# False
