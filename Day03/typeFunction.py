2fec6795513496e3845f683cb7b994e82945e241
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-02 22:37:55 +0530

    typeFunctions

---
+++ Day03/typeFunction.py
@@ -0,0 +1,18 @@
+a=10
+print(type(a))
+b="hello"
+print(type(b))
+c=5.5
+print(type(c))  
+d=True
+print(type(d))
+print(type(type(d)))
+e='e'
+print(type(e))
+
+# <class 'int' >
+# <class 'str' >
+# <class 'float' >
+# <class 'bool' >
+# <class 'type' >
+# <class 'str' >
