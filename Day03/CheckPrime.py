35b3c3dbb75a486e15a9f2f0b398cf63fd3b602e
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-11 20:53:03 +0530

    check prime numbar

---
+++ Day03/CheckPrime.py
@@ -0,0 +1,10 @@
+num = 29
+is_prime = True
+for i in range(2, int(num**0.5) + 1):
+    if num % i == 0:
+        is_prime = False
+        break
+if is_prime:
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
