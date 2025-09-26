123c1dc519ef5005eebc26d5d04f886d1222b208
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-26 11:57:40 +0530

    Tuples in python

--- Day02/tuple.py
+++ Day02/tuple.py
@@ -9,3 +9,10 @@ print(type(c))
 # 10
 # (10, '10')
 # <class 'tuple'>
+# Creating a tuple
+my_tuple = ("apple", "banana", "cherry")
+
+print("First fruit:", my_tuple[0])  # Output: apple
+
+for fruit in my_tuple:
+    print("Fruit:", fruit)
