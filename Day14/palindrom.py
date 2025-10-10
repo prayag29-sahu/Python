c693bd94b07062a84eccdcdc88ac31e48f551a6d
Prayag Sahu <sahuprayag229@gmail.com>
2025-10-10 16:01:27 +0530

    palindrom

--- Day14/palindrom.py
+++ Day14/palindrom.py
@@ -1,12 +1,18 @@
-a=121
-b=0
-c=a
-while a>0:
-    r=a%10
-    b=b*10+r
-    a=a//10
+# a=121
+# b=0
+# c=a
+# while a>0:
+#     r=a%10
+#     b=b*10+r
+#     a=a//10
 
-if b==c:
+# if b==c:
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+n=12321
+if str(n)[::-1]==str(n):
     print("palindrome")
 else:
-    print("not palindrome")
+    print("not palindrome")    
