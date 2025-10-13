bcd7e08fb36e53c44ed72c9a079672815415f11d
Prayag Sahu <sahuprayag229@gmail.com>
2025-10-13 19:53:58 +0530

    min heap

---
+++ Day16/minheap.py
@@ -0,0 +1,11 @@
+import heapq
+
+nums = [5, 3, 8, 1, 2]
+heapq.heapify(nums)
+print("Min Heap:", nums)
+
+heapq.heappush(nums, 0)
+print("After push:", nums)
+
+print("Popped element:", heapq.heappop(nums))
+print("Final heap:", nums)
