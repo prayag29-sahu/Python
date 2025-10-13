01752b19420262862160336fcbe0ab5c6ff21fee
Prayag Sahu <sahuprayag229@gmail.com>
2025-10-13 19:57:11 +0530

    Max heap popped operation

---
+++ Day16/maxheappoped.py
@@ -0,0 +1,12 @@
+import heapq
+
+nums = [4, 1, 7, 3, 8, 5]
+max_heap = [-n for n in nums]
+heapq.heapify(max_heap)
+
+print("Max Heap:", [-n for n in max_heap])
+heapq.heappush(max_heap, -10)
+print("After push:", [-n for n in max_heap])
+
+print("Max element popped:", -heapq.heappop(max_heap))
+print("Final heap:", [-n for n in max_heap])
