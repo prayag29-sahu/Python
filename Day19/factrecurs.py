a7b71d55812f07f8eb744db3bddde75bbe72ed48
Prayag Sahu <sahuprayag229@gmail.com>
2025-10-30 19:10:29 +0530

    factorial using recursion

---
+++ Day19/factrecurs.py
@@ -0,0 +1,18 @@
+
+
+# def rec(a):
+#     if a==1:
+#         return 1
+#     else: 
+#         return a*rec(a-1)
+    
+
+
+# recur = rec(5)
+# print (recur)/
+
+
+
+def fib(n):
+    if n==0 and n==1:
+        return 0
