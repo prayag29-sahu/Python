6bd659c4bd7f1c3579282b8ca4a86e1a6cd07078
Prayag Sahu <sahuprayag229@gmail.com>
2025-12-12 18:56:54 +0530

    Prime

---
+++ CodeforcePython/02_Sheet/H_Prime.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+if n <= 1:
+    print("NO")
+
+else:
+    for i in range(2, n):
+        if n % i == 0:
+            print("NO")
+            break
+    else:
+        print("YES")
