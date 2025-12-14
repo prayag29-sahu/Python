afb407a6b63ce3d1e8a43cc52ee7821d0e1a2160
Prayag Sahu <sahuprayag229@gmail.com>
2025-12-14 19:02:58 +0530

    some sum

---
+++ CodeforcePython/02_Sheet/U_SomeSum.py
@@ -0,0 +1,13 @@
+n, a, b = map(int, input().split())
+r1 = min(a, b)
+r2 = max(a, b)
+sum = 0
+for i in range(1, n+1):
+    temp = i
+    value = 0
+    while (temp > 0):
+        value = value+(temp % 10)
+        temp = temp//10
+    if (r1 <= value <= r2):
+        sum = sum+i
+print(sum)
