48083e682318f2025e78cc4d3e2915b4d00afee4
Prayag Sahu <sahuprayag229@gmail.com>
2025-12-17 17:23:11 +0530

    3 numbers

--- CodeforcePython/02_Sheet/Z_3Numbers.py
+++ CodeforcePython/02_Sheet/Z_3Numbers.py
@@ -1,12 +1,15 @@
-
 k, s = map(int, input().split())
-count = 0
-for x in range(0, k+1):
-    remaining = s-x
-    r1 = min(0, remaining-k)
-    r2 = max(k, remaining)
-    if r1 <= r2:
-        increase = (r2-r1)+1
-        count = count+increase
-print(count)
 
+count = 0
+for x in range(0, k + 1):
+    remaining = s - x
+    if remaining < 0:
+        continue
+
+    low = max(0, remaining - k)
+    high = min(k, remaining)
+
+    if low <= high:
+        count += (high - low + 1)
+
+print(count)
