56f69b8ae8e99cf3469801aaa678d42d5f625320
Prayag Sahu <sahuprayag229@gmail.com>
2025-12-19 21:32:51 +0530

    Contractasum

---
+++ CodeforcePython/02_Contest/G_ContractaSum.py
@@ -0,0 +1,21 @@
+t = int(input())
+
+for _ in range(t):
+    n, s = map(int, input().split())
+
+    max_sum = n * (n + 1) // 2
+    if s > max_sum:
+        print(-1)
+        continue
+
+    result = []
+    curr = s
+
+    for num in range(n, 0, -1):
+        if num <= curr:
+            result.append(num)
+            curr -= num
+        if curr == 0:
+            break
+
+    print(*result)
