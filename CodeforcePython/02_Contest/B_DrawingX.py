783ea9b719525c3597a58d690a723aa8b3abed7b
Prayag Sahu <sahuprayag229@gmail.com>
2025-12-21 18:44:55 +0530

    drawing x

--- CodeforcePython/02_Contest/B_DrawingX.py
+++ CodeforcePython/02_Contest/B_DrawingX.py
@@ -1,6 +1,5 @@
 n = int(input())
 m = n // 2
-
 for i in range(n):
     for j in range(n):
         if i == m and j == m:
@@ -12,3 +11,5 @@ for i in range(n):
         else:
             print("*", end="")
     print()
+
+
